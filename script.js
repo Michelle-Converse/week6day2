abc7af18c35453cecdfc4742971ec0ed3f1f29d3
Michelle-Converse <103612368+Michelle-Converse@users.noreply.github.com>
2022-05-18 23:09:53 -0600

    added main.html, script.js, style.css

---
+++ script.js
@@ -0,0 +1,63 @@
+$('#box1').on('click', function(){
+    $('#grid').toggleClass('lightBox1');
+});
+
+$('#box2').on('click', function(){
+    $('#grid').toggleClass('lightBox2');
+});
+
+$('#box3').on('click', function(){
+    $('#grid').toggleClass('lightBox3');
+});
+
+$('#box4').on('click', function(){
+    $('#grid').toggleClass('lightBox4');
+});
+
+$('#box5').on('click', function(){
+    $('#grid').toggleClass('lightBox5');
+});
+
+$('#box6').on('click', function(){
+    $('#grid').toggleClass('lightBox6');
+});
+
+$('#box7').on('click', function(){
+    $('#grid').toggleClass('lightBox7');
+});
+
+$('#box8').on('click', function(){
+    $('#grid').toggleClass('lightBox8');
+});
+
+$('#box9').on('click', function(){
+    $('#grid').toggleClass('lightBox9');
+});
+
+$('#box10').on('click', function(){
+    $('#grid').toggleClass('lightBox10');
+});
+
+$('#box11').on('click', function(){
+    $('#grid').toggleClass('lightBox11');
+});
+
+$('#box12').on('click', function(){
+    $('#grid').toggleClass('lightBox12');
+});
+
+$('#box13').on('click', function(){
+    $('#grid').toggleClass('lightBox13');
+});
+
+$('#box14').on('click', function(){
+    $('#grid').toggleClass('lightBox14');
+});
+
+$('#box15').on('click', function(){
+    $('#grid').toggleClass('lightBox15');
+});
+
+$('#box16').on('click', function(){
+    $('#grid').toggleClass('lightBox16');
+});
